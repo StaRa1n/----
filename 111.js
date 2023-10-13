35d054ae99ebab8154104453b481b0e204db70e5
zhangguiyang <117461727+shouzhuabing8@users.noreply.github.com>
2023-10-13 18:14:43 +0800

    Update 111.js

--- 111.js
+++ 111.js
@@ -1,3 +1,27 @@
+var firstMissingPositive = function(nums) {
+    function swap(nums, i, j) {
+        let temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
 
-console.log(n % 3)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1 && nums[i] > 0 && nums[i] < nums.length) {
+            // 将数组的值与下标对应起来
+            swap(nums, nums[i] - 1, i)
+            // 继续判断当前位置是否相对应
+            i--
+        }
+    }
 
+    console.log(nums)
+
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1
+        }
+    }
+    return nums.length + 1
+};
+
+firstMissingPositive([3,4,-1,1])
